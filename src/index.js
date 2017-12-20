73ad88af44844a5d4ab2f02c614e2a2ed33814fe
Brian Novak <bn@BMBP.local>
2017-12-20 12:07:50 -0700

    index.js update

--- src/index.js
+++ src/index.js
@@ -1,17 +1,42 @@
-const baseURL = "https://radiant-coast-65532.herokuapp.com/";
+const situationsURL = "http://localhost:3000/situations";
+const songsURL = "http://localhost:3000/songs";
+// const situationsURL = "https://w-here-dotcom-backend.herokuapp.com/situations";
+// const songsURL = "https://w-here-dotcom-backend.herokuapp.com/songs";
+let situationList = document.querySelector("select");
 
-fetch(baseURL)
-  .then(response => response.json())
-  .then(response => console.log(response))
-  .catch(err => console.log(err));
+document.onload = fetchSituationInfoFromAPI();
 
-fetch("https://radiant-coast-65532.herokuapp.com/",  {
-  method: "post",
-  body: JSON.stringify({greeting: "hello"}),
-  headers: new Headers({
-    "Content-Type": "application/json"
-  })
-})
-  .then(response => response.json())
-  .then(response => console.log(response))
-  .catch(err => console.log(err));
+function fetchSituationInfoFromAPI(){
+	fetch(situationsURL)
+	.then(response => response.json())
+	.then(response => {
+		console.log(response);
+		sortSituations(response);
+		console.log(response);
+		createOptions(response);
+	})
+	.catch(console.error);
+}
+
+function fetchSongInfoFromAPI(){
+	fetch(songsURL)
+	.then(response => response.json())
+	.then(response => {
+		listRescues(filteredRescueData);
+	})
+	.catch(console.error);
+}
+
+function sortSituations(response){
+	return response.sort((a,b) => a.name-b.name);
+}
+
+function createOptions(situationData){
+	for (var i = 0; i < situationData.length; i++) {
+		var option = document.createElement("option");
+		option.id = situationData[i].id;
+		option.setAttribute("value", situationData[i].Name);
+		option.innerText = situationData[i].Name;
+		situationList.appendChild(option);
+	}
+}
