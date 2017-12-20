e6e197291f861ffdc1c926000e6f8735e8ab3982
Brian Novak <bn@BMBP.local>
2017-12-20 14:28:34 -0700

    index.js updated kinda but not working

--- src/index.js
+++ src/index.js
@@ -1,17 +1,28 @@
-const situationsURL = "http://localhost:3000/situations";
-const songsURL = "http://localhost:3000/songs";
-// const situationsURL = "https://https://w-here-dotcom.herokuapp.com/situations";
-// const songsURL = "https://w-here-dotcom.herokuapp.com/songs";
-const button = document.querySelector("button");
-const videoDiv = document.querySelector(".video");
+const situationsURL = "https://w-here-dotcom.herokuapp.com/situations";
+const songsURL = "https://w-here-dotcom.herokuapp.com/songs";
+const button = document.querySelector(".button");
+const videoDiv = document.querySelector("iframe");
 let situationList = document.querySelector("select");
+let chosenSituation;
+let situationID;
 
 document.onload = fetchSituationInfoFromAPI();
-button.addEventListener("click", (event) => {
-	let chosenSituation = event.target;
+situationList.onchange = changeEventHandler;
+
+button.addEventListener("click", () => {
 	console.log(chosenSituation);
+	console.log(situationID);
+	fetch(songsURL)
+	.then(response => response.json())
+	.then(response => {
+		console.log(response);
+	});
 });
-situationList.onchange = fetchSongInfoFromAPI(event);
+
+function changeEventHandler(event){
+	chosenSituation = event.target.value;
+	situationID = situationList[situationList.selectedIndex].id;
+}
 
 function fetchSituationInfoFromAPI(){
 	fetch(situationsURL)
@@ -27,13 +38,17 @@ function fetchSongInfoFromAPI(){
 	.then(response => response.json())
 	.then(response => {
 		console.log(response);
+		// let selectedSong = response.filter(song => song.id == situationID);
+		// console.log(selectedSong);
+		// console.log(chosenSituation);
+		// console.log(situationID);
 		//displaySongInfo(response);
 	})
 	.catch(console.error);
 }
 
 function displaySongInfo(responseData){
-
+	console.log(responseData);
 }
 
 function createOptions(situationData){
